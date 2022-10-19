976bb8827c115f293a79641898f874ee6e0b4a3d
pp957 <pp9575@yandex.ru>
2022-10-20 00:28:17 +0300

    DZ4 done

---
+++ src/test/java/maven_tutorial/integration/BrickFactoryIT.java
@@ -0,0 +1,37 @@
+package maven_tutorial.integration;
+
+import lombok.SneakyThrows;
+import org.example.module_3.lesson7.homework.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrickFactoryIT {
+
+    @Test
+    public void tiredWorkerTest() {
+        Worker worker = new Worker(true);
+        BrickFactory brickFactory = new BrickFactory(worker, 1000, 1000);
+        Assertions.assertThrows(WorkerTiredException.class, brickFactory::createBrick);
+    }
+
+    @Test
+    public void brickFactoryExceptionTest() {
+        Worker worker = new Worker(false);
+        BrickFactory brickFactory = new BrickFactory(worker, 500, 100);
+        Assertions.assertThrows(NotEnoughResourcesException.class, brickFactory::createBrick);
+    }
+
+    @Test
+    @SneakyThrows
+    public void twoBricksTest() {
+        Worker worker = new Worker(false);
+        BrickFactory brickFactory = new BrickFactory(worker, 2000, 2000);
+        List<Brick> bricks = new ArrayList<>();
+        bricks.add(brickFactory.createBrick());
+        bricks.add(brickFactory.createBrick());
+        Assertions.assertEquals(2, bricks.size());
+    }
+}
