cd1209bb689bfed3218d9adf101cb9d1ec16f421
Ablazzing <y2286122@yandex.ru>
2022-10-21 15:35:17 +0300

    Лекция №5

--- src/test/java/maven_tutorial/CalcTest.java
+++ src/test/java/maven_tutorial/CalcTest.java
@@ -6,10 +6,12 @@ import org.junit.Test;
 public class CalcTest {
 
     @Test
+
     public void myFirstTest() {
         int result = Calc.addNumbers(1, 2);
         if (result != 3) {
             throw new RuntimeException("Ошибка! Результат не соответствует ожидаемому!");
         }
+
     }
 }
